3e595b2e85990ded5691b2d0647d56ffce3a96a7
jimmy.hui <jimmy.hui@1997749661648079.onaliyun.com>
2024-08-27 15:18:46 -0700

    feat: add max-width

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,9 +3,11 @@ import VoteWrap from '../../components/vote/VoteWrap';
 function Home() {
 
     return (
-        <div className="mx-[15px] my-[15px]">
-            <h1>Vote</h1>
-            <VoteWrap />
+        <div className='max-w-[768px] m-auto'>
+            <div className="mx-[15px] my-[15px]">
+                <h1>Vote</h1>
+                <VoteWrap />
+            </div>
         </div>
     );
 }
